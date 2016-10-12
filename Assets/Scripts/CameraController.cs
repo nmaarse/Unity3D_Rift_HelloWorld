456b8787998d2fe56789265f5410fbb60c627a65
johanvanommen <j.vanommen@nl.ccv.eu>
2016-10-12 15:16:55 +0200

    Movable Camera?

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,15 @@ using System.Collections;
 public class CameraController : MonoBehaviour 
 
 {
-	public GameObject player;
-	private Vector3 offset;
-	// Use this for initialization
-	void Start () {
-		offset = transform.position;
-	}
+	//public GameObject player;
+	//private Vector3 offset;
+	//// Use this for initialization
+	//void Start () {
+	//	offset = transform.position;
+	//}
 	
-	// Update is called once per frame
-	void LateUpdate () {
-		transform.position = player.transform.position + offset;
-	}
+	//// Update is called once per frame
+	//void LateUpdate () {
+	//	transform.position = player.transform.position + offset;
+	//}
 }
---
+++ Assets/Scripts/MovableCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class MovableCamera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //InputTracking.GetLocalPosition(VRNode.Head);
+        transform.position = InputTracking.GetLocalPosition(VRNode.Head) + offset;
+    }
+
+    
+}
